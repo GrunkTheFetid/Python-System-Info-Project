c446c84dde300801fc0f5709a0c2c6c5c2dc5e68
Cade <76876189+GrunkTheFetid@users.noreply.github.com>
2021-01-30 17:25:34 -0500

    Update SysInfo.py

--- SysInfo.py
+++ SysInfo.py
@@ -3,17 +3,50 @@ import psutil
 import cpuinfo
 
 window = tk.Tk()
+window.title("System Info")
+window.configure(bg="white")
 
 cpu = tk.Label(
-    text=cpuinfo.get_cpu_info()["brand_raw"]
-    +"\n Cores: "+str(psutil.cpu_count(logical=False))
-    +"\n Threads: "+str(psutil.cpu_count(logical=True))
-    +"\n Speed: "+cpuinfo.get_cpu_info()["hz_actual_friendly"],
+    text="",
     foreground="black",
     background="white",
     width=40,
-    height=20,
-    font="Arial"
+    height=10,
+    font="Arial",
+    justify="left"
     )
 cpu.pack()
+
+mem = tk.Label(
+    text="",
+    foreground="black",
+    background="white",
+    width=40,
+    height=10,
+    font="Arial",
+    justify="left"
+    )
+mem.pack()
+
+def Refresh():
+    cpu["text"]=(
+        cpuinfo.get_cpu_info()["brand_raw"]
+        +"\n\n Cores: "+str(psutil.cpu_count(logical=False))
+        +"\n Threads: "+str(psutil.cpu_count(logical=True))
+        +"\n Speed: "+cpuinfo.get_cpu_info()["hz_actual_friendly"]
+        )
+
+    totalmem = str(round(psutil.virtual_memory().total / (1024 ** 3),2))+" GB"
+    MemUsed = str(round(psutil.virtual_memory().used / (1024 ** 3),2))+" GB"
+    MemUsedPercent = str(round(psutil.virtual_memory().percent,1))
+
+    mem["text"]=(
+        "Memory"
+        +f"\n\nTotal: {totalmem}"
+        +f"\nIn Use: {MemUsed} ({MemUsedPercent}%)"
+        )
+    
+    window.after(1000,Refresh) 
+
+Refresh()
 window.mainloop()
