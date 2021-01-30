7da866d0d28c4ff1fd238ff1594bc3620e9ad409
Cade <76876189+GrunkTheFetid@users.noreply.github.com>
2021-01-30 15:50:07 -0500

    Code Organization

--- SysInfo.py
+++ SysInfo.py
@@ -5,7 +5,10 @@ import cpuinfo
 window = tk.Tk()
 
 cpu = tk.Label(
-    text=cpuinfo.get_cpu_info()["brand_raw"]+"\n Cores: "+str(psutil.cpu_count(logical=False))+"\n Threads: "+str(psutil.cpu_count(logical=True))+"\n Speed: "+cpuinfo.get_cpu_info()["hz_actual_friendly"],
+    text=cpuinfo.get_cpu_info()["brand_raw"]
+    +"\n Cores: "+str(psutil.cpu_count(logical=False))
+    +"\n Threads: "+str(psutil.cpu_count(logical=True))
+    +"\n Speed: "+cpuinfo.get_cpu_info()["hz_actual_friendly"],
     foreground="black",
     background="white",
     width=40,
